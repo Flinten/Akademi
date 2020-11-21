843993400e18b6be413f89df9201119a91f0ca7a
Mads Olsen <mol@skm.dk>
2020-11-21 08:12:17 +0100

    Lidt ændringer for at gøre jeres kode klar til at arbejde videre med mandag: - Hot-reload hvor ændringer oftere slår igennem i browseren uden reload - :onClick ændret til :on-click som er en standard måden at skrive det på - slettet ubenyttet kode - indsat (doall ...) rundt om (for ...), da lazyness ellers kan give problemer - ændret draw-objs tilbage til at være defonce - ellers nulstilles data i browseren efter hver kodeændring

--- src/main/akademi/core.cljs
+++ src/main/akademi/core.cljs
@@ -2,59 +2,57 @@
  (:require [reagent.core :as r]
            [reagent.dom :as rdom]))
 (defonce a (r/atom ["Steen" "Casper" ]))
+(defonce skin (r/atom :basic))
 (defonce debug-mode (r/atom false))
-(def draw-objs ;
+(defonce draw-objs ;
   (r/atom [{:id 1 :type :box :pos [100,100], :size [50,50]}]))
-;-------------------simpel tekst eksempel-------
-(defn simple-component []
-  [:div
-   [:p "Jeg er en smuk lille komponent"] 
-   [:ul (for[x @a] [:li x])]
-   [:button {:onClick (fn[event] 
-                        (.log js/console event)
-                        (swap! a conj (str "person " (count @a))))}"Jeg er en lille sød knap"]
-   [:p.someclass
-    "Jeg kan skrive i " [:strong "bold"]
-    [:span {:style {:color "red"}} " og rød "] "text."]])
+
 (def render-fns {:default {:box (fn[{[x y] :pos}] 
                                 [:rect {:x x :y y,  
                                         :width 50 :height 20 
-                                        :style {:fill :green :stroke :blue}}])}})
+                                        :style {:fill :green :stroke :blue}}])}
+                 :basic {:box (fn [{[x y] :pos [w h] :size :as ob}]
+                                  [:rect {:x x :y y :on-click #(js/alert (pr-str ob))
+                                          :width w :height h
+                                          :style {:fill :khaki :stroke :blue}}])}})
 
 ;______________tegneflade_________________
-;------Objecter-----
-(defn d-obj[{:keys [id type pos size]}]
-  (let[[x y ]pos]
-  (if @debug-mode 
-    [:div (str type " objekt " id)]
-    [:rect {:key id, :x x :y y,  :width 50 :height 20 :style {:fill :red :stroke :black} }]))
-  )
  ;Tilføj nyt box objekt der bliver placeret på random pos
 (defn new-box[obj-list]
   (conj obj-list {:id (inc(apply max (map :id obj-list))) :type :box :pos [(rand-int 500),(rand-int 500)] :size [20 20]})
   )
 (defn control-area []
   [:div 
-   [:button {:onClick (fn [_] 
-                             (swap! draw-objs new-box))} "Ny kasse"]
-   [:button {:onClick #(swap! debug-mode not)}"debug on/off"]
+   [:button {:on-click (fn [_] (swap! draw-objs new-box))} "Ny kasse"]
+   [:button {:on-click #(swap! debug-mode not)}"debug on/off"]
+   [:label "Skin:"
+    [:select {:selected (str @skin) 
+              :on-change (fn[event] (reset! skin (keyword (.-value (.-target event)))))} 
+     (doall (for [x (sort (keys render-fns))]
+              ^{:key (name x)} [:option (name x)]))]]
    ]
   )
 
 (defn render-obj-debug [obj] [:div (pr-str obj)])
 
 (defn draw-area [xs] ;xs er en collection 
-  (let [objs (for [x xs]
-               ^{:key (:id x)} [ (or
-                                  (when @debug-mode render-obj-debug)
-                                  (get-in render-fns [:default (:type x)])
-                                  (fn[_] [:div "Ukendt obj"])) x])]
+  (let [objs (doall 
+              (for [x xs]
+               ^{:key (:id x)} [(or
+                                 (when @debug-mode render-obj-debug)
+                                 (get-in render-fns [@skin (:type x)])
+                                 (get-in render-fns [:default (:type x)])
+                                 (fn [_] [:div "Ukendt obj"])) x]))]
     (if @debug-mode
      [:div objs]
-      [:svg {:width 500, :height 500, :style {:background-color :black}} objs])))
+      [:svg {:width 500, :height 500, :style {:background-color :linen}} objs])))
 (defn mini-app[]
 [:div [control-area]
- [draw-area @draw-objs]]
-)
+ [draw-area @draw-objs]])
+
 (defn ^:export run []
  (rdom/render [mini-app] (js/document.getElementById "app")))
+
+(defn ^:export reload []
+    (.log js/console "reload...")
+    (run))
