425246f7a303bc808f7caa395c2286821e813d6d
Jakob Zimsen <Jakob.Zimsen@ufst.dk>
2020-11-25 10:08:19 +0100

    Tilføj af flyt knap

--- src/main/akademi/core.cljs
+++ src/main/akademi/core.cljs
@@ -1,58 +1,61 @@
 (ns akademi.core
- (:require [reagent.core :as r]
-           [reagent.dom :as rdom]))
-(defonce a (r/atom ["Steen" "Casper" ]))
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+(defonce a (r/atom ["Steen" "Casper"]))
 (defonce skin (r/atom :basic))
 (defonce debug-mode (r/atom false))
 (defonce draw-objs ;
   (r/atom [{:id 1 :type :box :pos [100,100], :size [50,50]}]))
 
-(def render-fns {:default {:box (fn[{[x y] :pos}] 
-                                [:rect {:x x :y y,  
-                                        :width 50 :height 20 
-                                        :style {:fill :green :stroke :blue}}])}
+(def render-fns {:default {:box (fn [{[x y] :pos}]
+                                  [:rect {:x x :y y
+                                          :width 50 :height 20
+                                          :style {:fill :green :stroke :blue}}])}
                  :basic {:box (fn [{[x y] :pos [w h] :size :as ob}]
-                                  [:rect {:x x :y y :on-click #(js/alert (pr-str ob))
-                                          :width w :height h
-                                          :style {:fill :khaki :stroke :blue}}])}})
+                                [:rect {:x x :y y :on-click #(js/alert (pr-str ob))
+                                        :width w :height h
+                                        :style {:fill :khaki :stroke :blue}}])}})
 
 ;______________tegneflade_________________
  ;Tilføj nyt box objekt der bliver placeret på random pos
-(defn new-box[obj-list]
-  (conj obj-list {:id (inc(apply max (map :id obj-list))) :type :box :pos [(rand-int 500),(rand-int 500)] :size [20 20]})
-  )
+(defn new-box [obj-list]
+  (conj obj-list {:id (inc (apply max (map :id obj-list))) :type :box :pos [(rand-int 500),(rand-int 500)] :size [20 20]}))
+
+(defn align-left "Flytter objecter til venster" [xs]
+  (map (fn [{[_ y] :pos :as obj}]
+         (assoc obj :pos [0 y])) xs))
+
 (defn control-area []
-  [:div 
+  [:div
    [:button {:on-click (fn [_] (swap! draw-objs new-box))} "Ny kasse"]
-   [:button {:on-click #(swap! debug-mode not)}"debug on/off"]
+   [:button {:on-click #(swap! draw-objs align-left)} "Flyt kasser"]
+   [:button {:on-click #(swap! debug-mode not)} "debug on/off"]
    [:label "Skin:"
-    [:select {:selected (str @skin) 
-              :on-change (fn[event] (reset! skin (keyword (.-value (.-target event)))))} 
+    [:select {:selected (str @skin)
+              :on-change (fn [event] (reset! skin (keyword (.-value (.-target event)))))}
      (doall (for [x (sort (keys render-fns))]
-              ^{:key (name x)} [:option (name x)]))]]
-   ]
-  )
+              ^{:key (name x)} [:option (name x)]))]]])
 
 (defn render-obj-debug [obj] [:div (pr-str obj)])
 
 (defn draw-area [xs] ;xs er en collection 
-  (let [objs (doall 
+  (let [objs (doall
               (for [x xs]
-               ^{:key (:id x)} [(or
-                                 (when @debug-mode render-obj-debug)
-                                 (get-in render-fns [@skin (:type x)])
-                                 (get-in render-fns [:default (:type x)])
-                                 (fn [_] [:div "Ukendt obj"])) x]))]
+                ^{:key (:id x)} [(or
+                                  (when @debug-mode render-obj-debug)
+                                  (get-in render-fns [@skin (:type x)])
+                                  (get-in render-fns [:default (:type x)])
+                                  (fn [_] [:div "Ukendt obj"])) x]))]
     (if @debug-mode
-     [:div objs]
+      [:div objs]
       [:svg {:width 500, :height 500, :style {:background-color :linen}} objs])))
-(defn mini-app[]
-[:div [control-area]
- [draw-area @draw-objs]])
+(defn mini-app []
+  [:div [control-area]
+   [draw-area @draw-objs]])
 
 (defn ^:export run []
- (rdom/render [mini-app] (js/document.getElementById "app")))
+  (rdom/render [mini-app] (js/document.getElementById "app")))
 
 (defn ^:export reload []
-    (.log js/console "reload...")
-    (run))
+  (.log js/console "reload...")
+  (run))
